7f4897aed1a6fbcb0204661972f06f175420e59d
JohnS3223 <jsehuwerert@aol.com>
2022-04-15 18:33:16 -0400

    WIP

--- main.py
+++ main.py
@@ -14,34 +14,52 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center; color: coral;'>Animal Crossing Fishpedia!</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: coral;'>🐟 Animal Crossing Fishpedia! 🐟</h1>", unsafe_allow_html=True)
 st.markdown("<p class='short-font'>A comprehensive resource of all of the "
             "fish available in Animal Crossing: New Horizons </p>", unsafe_allow_html=True)
 
-fishAPI = "http://acnhapi.com/v1/fish/80"
-fish = requests.get(fishAPI).json()
+
 #st.write(fish)
 
 hemispheres = ["Northern", "Southern"]
 fish_hemisphere = st.radio("Which hemisphere do you play on?", hemispheres, index=0)
 
+def fish_data():
+    choice = st.checkbox("Check here to input a specific fish ID!", key=0)
+    if choice:
+        entry_no = st.number_input("Which fish are you looking for?", min_value=1, max_value=80, value=1, format='%d')
+        if(entry_no):
+            fishAPI = "http://acnhapi.com/v1/fish/" + str(entry_no)
+            fish = requests.get(fishAPI).json()
+
+            st.write("Essential " + str(fish["name"]["name-USen"]) + " information")
+            st.write(fish)
+            st.image(fish["icon_uri"])
+            st.write("Regular Selling Price")
+            st.write(str(fish["price"]) + " Bells")
+            st.write("CJ's Offer")
+            st.write(str(fish["price-cj"]) + " Bells")
+            result = st.button("Additional Fish Information")
+            if result:
+                df = pd.DataFrame(
+                    fish["name"],
+                    index=["Names"])
+
+                st.success("Enjoy your fish data!")
+                st.dataframe(df)
+                st.write("Fish Shadow Size in Water: ", fish["shadow"])
+                st.image(fish["image_uri"], caption=fish["catch-phrase"])
+                st.write(fish["museum-phrase"])
+
+
+
 if fish_hemisphere == "Northern":
-    st.success("NORTH")
     nh = pd.DataFrame(
         np.random.randn(1000, 2) / [45, 45] + [45, 0],
         columns=['lat', 'lon']
     )
     st.map(nh, zoom=None)
-    choice = st.checkbox("Check here to input a specific fish ID!")
-    if choice:
-        entry_no = st.number_input("Which fish are you looking for?", min_value=1, max_value=80, value=1, format='%d')
-        st.write("You have entered fish #", entry_no)
-        if(entry_no):
-            result = st.button("Additional Fish Information")
-            if result:
-                st.write("Success!")
-
-    #fish_option = st.selectbox("Choose the fish you're looking for!", )
+    fish_data()
 else:
     st.success("SOUTH")
     sh = pd.DataFrame(
@@ -49,14 +67,5 @@ else:
         columns=['lat', 'lon']
     )
     st.map(sh, zoom=None)
-    choice = st.checkbox("Check here to input a specific fish ID!")
-    if choice:
-        entry_no = st.number_input("Which fish are you looking for?", min_value=1, max_value=80, value=1, format='%d')
-        st.write("You have entered fish #", entry_no)
-    #fish_option = st.selectbox("Choose the fish you're looking for!", )
-        if(entry_no):
-            result = st.button("Additional Fish Information")
-            if result:
-                st.write("Success!")
-
+    fish_data()
 
